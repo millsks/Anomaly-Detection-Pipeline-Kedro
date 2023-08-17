f959bd40c4c16710f7b204f8a2e859bf5c7ededb
Kevin Mills <millsks@gmail.com>
2023-08-17 11:46:12 -0500

    Remove neptune from model_evaluation/nodes.py
    Signed-off-by: Kevin Mills <millsks@gmail.com>

--- src/anomaly_detection_pipeline_kedro/pipelines/model_evaluation/nodes.py
+++ src/anomaly_detection_pipeline_kedro/pipelines/model_evaluation/nodes.py
@@ -8,20 +8,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, precision_recall_curve, auc
 import logging
-import neptune.new as neptune
 
-run = neptune.init(project='kennethleung.ty/Anomaly-Detection-Pipeline-Kedro',
-                   api_token='<API_TOKEN>')
 
-def evaluate_model(predictions: pd.DataFrame, test_labels: pd.DataFrame,
-                   neptune_run: run):
+def evaluate_model(predictions: pd.DataFrame, test_labels: pd.DataFrame):
     def get_auc(labels, scores):
-        fpr, tpr, thr = roc_curve(labels, scores)
+        fpr, tpr, _ = roc_curve(labels, scores)
         auc_score = auc(fpr, tpr)
         return fpr, tpr, auc_score
 
     def get_aucpr(labels, scores):
-        precision, recall, thr = precision_recall_curve(labels, scores)
+        precision, recall, _ = precision_recall_curve(labels, scores)
         aucpr_score = np.trapz(recall, precision)
         return precision, recall, aucpr_score
 
@@ -57,20 +53,13 @@ def evaluate_model(predictions: pd.DataFrame, test_labels: pd.DataFrame,
     _, _, auc_score = get_auc(test_labels['TX_FRAUD'].values,  predictions['ANOMALY_SCORE'].values)
     _, _, aucpr_score = get_aucpr(test_labels['TX_FRAUD'].values,  predictions['ANOMALY_SCORE'].values)
 
-    # log = logging.getLogger(__name__)
-    # log.info("AUC-ROC Score: %0.2f%%", auc_score)
-    # log.info("AUC-PR Score: %0.2f%%", aucpr_score)
-
-    # Log scores into Neptune
-    neptune_run['nodes/report/auc_roc_score'].log(auc_score)
-    neptune_run['nodes/report/auc_pr_score'].log(aucpr_score)
+    log = logging.getLogger(__name__)
+    log.info("AUC-ROC Score: %0.2f%%", auc_score)
+    log.info("AUC-PR Score: %0.2f%%", aucpr_score)
 
     fig = plt.figure()
     fig.set_figheight(4.5)
     fig.set_figwidth(4.5*2)
-    axes = prediction_summary(test_labels['TX_FRAUD'].values, predictions['ANOMALY_SCORE'].values, "Isolation Forest")
-
-    # Log AUC plots to Neptune
-    neptune_run['nodes/report/auc_plots'].upload(fig)
+    _ = prediction_summary(test_labels['TX_FRAUD'].values, predictions['ANOMALY_SCORE'].values, "Isolation Forest")
 
     return fig
